79ad2eebc1680a54ab1774be534f76332265ca70
José María Sosa <josemaria@rigs.com.mx>
2019-07-05 12:27:07 -0500

    My music project! 🎹

---
+++ music.py
@@ -0,0 +1,437 @@
+music = {
+    ""
+}
+
+# https://www.pianoscales.org/major-harmonizing.html
+
+[
+    {
+        "root": "a",
+        "major": ["a", "b", "c#", "d", "e", "f#", "g#", "a"],
+        "minor": {
+            "natural": ["a", "b", "c", "d", "e", "f", "g", "a"],
+            "harmonic": ["a", "b", "c", "d", "e", "f", "g#", "a"],
+            "melodic": ["a", "b", "c", "d", "e", "f#", "g#", "a"]
+        },
+        "harmonization": {
+            "major": [
+                {
+                    "mode": None,
+                    "degree": "I",
+                    "name": "A",
+                    "full_name": "a major"
+                    "notes": {
+                        "triad": ["a", "c#", "e"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "ii",
+                    "name": "Bmin",
+                    "full_name": "b minor"
+                    "notes": {
+                        "triad": ["b", "d", "f#"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "iii",
+                    "name": "C#min",
+                    "full_name": "c# minor"
+                    "notes": {
+                        "triad": ["c#", "e", "g#"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "IV",
+                    "name": "D",
+                    "full_name": "d major"
+                    "notes": {
+                        "triad": ["d", "f#", "a"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "V",
+                    "name": "E",
+                    "full_name": "e major"
+                    "notes": {
+                        "triad": ["e", "g#", "b"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "vi",
+                    "name": "F#min",
+                    "full_name": "f# minor"
+                    "notes": {
+                        "triad": ["f#", "a", "c#"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "vii°",
+                    "name": "G#dim",
+                    "full_name": "g# diminished"
+                    "notes": {
+                        "triad": ["g#", "b", "d"]
+                    }
+                }
+            ],
+            "minor": {
+                "natural": [
+                    {
+                        "mode": None,
+                        "degree": "i",
+                        "name": "Amin",
+                        "full_name": "a minor"
+                        "notes": {
+                            "triad": ["a", "c", "e"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "ii°",
+                        "name": "Bdim",
+                        "full_name": "b diminished"
+                        "notes": {
+                            "triad": ["b", "d", "f"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "III",
+                        "name": "C",
+                        "full_name": "c major"
+                        "notes": {
+                            "triad": ["c", "e", "g"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "iv",
+                        "name": "Dmin",
+                        "full_name": "d minor"
+                        "notes": {
+                            "triad": ["d", "f", "a"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "v",
+                        "name": "Emin",
+                        "full_name": "e minor"
+                        "notes": {
+                            "triad": ["e", "g", "b"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "VI",
+                        "name": "F",
+                        "full_name": "f major"
+                        "notes": {
+                            "triad": ["f", "a", "c"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "VII",
+                        "name": "G",
+                        "full_name": "g major"
+                        "notes": {
+                            "triad": ["g", "b", "d"]
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    {
+        "root": "a#/bb",
+        "major": ["a#", "c", "d", "d#", "f", "g", "a", "a#"],
+        "minor": {
+            "natural": ["a#", "b#", "c#", "d#", "e#", "f#", "g#", "a#"],
+            "harmonic": ["a#", "b#", "c#", "d#", "e#", "f#", "a", "a#"],
+            "melodic": ["a#", "b#", "c#", "d#", "e#", "g", "a", "a#"]
+        },
+        "harmonization": {
+            "major": [
+                {
+                    "mode": None,
+                    "degree": "I",
+                    "name": "A#",
+                    "full_name": "a# major"
+                    "notes": {
+                        "triad": ["a#", "d", "f"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "ii",
+                    "name": "Cmin",
+                    "full_name": "c minor"
+                    "notes": {
+                        "triad": ["c", "d#", "g"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "iii",
+                    "name": "Dmin",
+                    "full_name": "d minor"
+                    "notes": {
+                        "triad": ["d", "f", "a"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "IV",
+                    "name": "D#",
+                    "full_name": "d# major"
+                    "notes": {
+                        "triad": ["d#", "g", "a#"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "V",
+                    "name": "F",
+                    "full_name": "f major"
+                    "notes": {
+                        "triad": ["f", "a", "c"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "vi",
+                    "name": "Gmin",
+                    "full_name": "g minor"
+                    "notes": {
+                        "triad": ["g", "a#", "d"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "vii°",
+                    "name": "Adim",
+                    "full_name": "A diminished"
+                    "notes": {
+                        "triad": ["a", "c", "d#"]
+                    }
+                }
+            ],
+            "minor": {
+                "natural": [
+                    {
+                        "mode": None,
+                        "degree": "i",
+                        "name": "A#min",
+                        "full_name": "a# minor"
+                        "notes": {
+                            "triad": ["a#", "c#", "e#"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "ii°",
+                        "name": "B#dim",
+                        "full_name": "b# diminished"
+                        "notes": {
+                            "triad": ["b#", "d#", "f#"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "III",
+                        "name": "C#",
+                        "full_name": "c# major"
+                        "notes": {
+                            "triad": ["c#", "e#", "g#"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "iv",
+                        "name": "D#min",
+                        "full_name": "d# minor"
+                        "notes": {
+                            "triad": ["d#", "f#", "a#"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "v",
+                        "name": "E#min",
+                        "full_name": "e# minor"
+                        "notes": {
+                            "triad": ["e#", "g#", "b#"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "VI",
+                        "name": "F#",
+                        "full_name": "f# major"
+                        "notes": {
+                            "triad": ["f#", "a#", "c#"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "VII",
+                        "name": "G#",
+                        "full_name": "g# major"
+                        "notes": {
+                            "triad": ["g#", "b#", "d#"]
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    {
+        "root": "b",
+        "major": ["b", "c#", "d#", "e", "f#", "g#", "a#", "b"],
+        "minor": {
+            "natural": ["b", "c#", "d", "e", "f#", "g", "a", "b"],
+            "harmonic": ["b", "c#", "d", "e", "f#", "g", "a#", "b"],
+            "melodic": ["b", "c#", "d", "e", "f#", "g#", "a#", "b"]
+        },
+        "harmonization": {
+            "major": [
+                {
+                    "mode": None,
+                    "degree": "I",
+                    "name": "B",
+                    "full_name": "b major"
+                    "notes": {
+                        "triad": ["b", "d#", "f#"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "ii",
+                    "name": "C#min",
+                    "full_name": "c# minor"
+                    "notes": {
+                        "triad": ["c#", "e", "g#"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "iii",
+                    "name": "D#min",
+                    "full_name": "d# minor"
+                    "notes": {
+                        "triad": ["d#", "f#", "a#"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "IV",
+                    "name": "E",
+                    "full_name": "e major"
+                    "notes": {
+                        "triad": ["e", "g#", "b"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "V",
+                    "name": "F#",
+                    "full_name": "f# major"
+                    "notes": {
+                        "triad": ["f#", "a#", "c#"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "vi",
+                    "name": "G#min",
+                    "full_name": "g# minor"
+                    "notes": {
+                        "triad": ["g#", "b", "d#"]
+                    }
+                },
+                {
+                    "mode": None,
+                    "degree": "vii°",
+                    "name": "A#dim",
+                    "full_name": "A# diminished"
+                    "notes": {
+                        "triad": ["a#", "c#", "e"]
+                    }
+                }
+            ],
+            "minor": {
+                "natural": [
+                    {
+                        "mode": None,
+                        "degree": "i",
+                        "name": "Bmin",
+                        "full_name": "b minor"
+                        "notes": {
+                            "triad": ["b", "d", "f#"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "ii°",
+                        "name": "C#dim",
+                        "full_name": "c# diminished"
+                        "notes": {
+                            "triad": ["c#", "e", "f"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "III",
+                        "name": "D",
+                        "full_name": "d major"
+                        "notes": {
+                            "triad": ["d", "f#", "a"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "iv",
+                        "name": "Emin",
+                        "full_name": "e minor"
+                        "notes": {
+                            "triad": ["e", "g", "b"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "v",
+                        "name": "F#min",
+                        "full_name": "f# minor"
+                        "notes": {
+                            "triad": ["f#", "a", "c#"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "VI",
+                        "name": "G",
+                        "full_name": "g major"
+                        "notes": {
+                            "triad": ["g", "b", "d"]
+                        }
+                    },
+                    {
+                        "mode": None,
+                        "degree": "VII",
+                        "name": "A",
+                        "full_name": "a major"
+                        "notes": {
+                            "triad": ["a", "c#", "e"]
+                        }
+                    }
+                ]
+            }
+        }
+    }
+]
